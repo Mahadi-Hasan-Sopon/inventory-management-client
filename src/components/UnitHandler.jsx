e357cd5a333c9248fc7616dfc1c8d50434461f4c
Mahadi Hasan <mahadih137@gmail.com>
2024-05-14 11:55:35 +0600

    checkout view updated to table view

---
+++ src/components/UnitHandler.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
+import "./unitHandler.css";
+
+const UnitHandler = ({ productStockQuantity, defaultSelectedQuantity }) => {
+  const [selectedQuantity, setSelectedQuantity] = useState(
+    defaultSelectedQuantity
+  );
+
+  const handleIncrementClick = () => {
+    if (selectedQuantity < productStockQuantity) {
+      setSelectedQuantity(selectedQuantity + 1);
+    } else {
+      setSelectedQuantity(productStockQuantity);
+    }
+  };
+
+  const handleDecrementClick = () => {
+    if (selectedQuantity > 1) {
+      setSelectedQuantity(selectedQuantity - 1);
+    } else {
+      setSelectedQuantity(1);
+    }
+  };
+
+  return (
+    <div className="custom-number-input h-10 w-32">
+      <div className="flex flex-row h-10 w-full rounded-lg relative bg-transparent mt-1">
+        <button
+          data-action="decrement"
+          className=" bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-l cursor-pointer outline-none"
+          onClick={handleDecrementClick}
+        >
+          <span className="m-auto text-2xl font-thin">−</span>
+        </button>
+        <input
+          type="number"
+          className="outline-none focus:outline-none text-center w-full bg-gray-300 font-semibold text-md hover:text-black focus:text-black  md:text-basecursor-default flex items-center text-gray-700"
+          name="custom-input-number"
+          value={selectedQuantity}
+        ></input>
+        <button
+          data-action="increment"
+          className="bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-r cursor-pointer"
+          onClick={handleIncrementClick}
+        >
+          <span className="m-auto text-2xl font-thin">+</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default UnitHandler;
+
+UnitHandler.propTypes = {
+  productStockQuantity: PropTypes.number.isRequired,
+  defaultSelectedQuantity: PropTypes.number.isRequired,
+};
--- src/pages/dashboard/checkout/CheckoutCart.jsx
+++ src/pages/dashboard/checkout/CheckoutCart.jsx
@@ -1,50 +1,107 @@
 import { Helmet } from "react-helmet";
 import { Link } from "react-router-dom";
 import useCart from "../../../hooks/useCart";
+import { MdDelete } from "react-icons/md";
+import UnitHandler from "../../../components/UnitHandler";
 
 const CheckoutCart = () => {
   const { cartItems, isLoading } = useCart();
 
+  console.log(cartItems?.totalItems);
+
   return (
     <div className="min-h-[62vh]">
       <Helmet>
-        <title>Inventory || Checkout Cart</title>
+        <title>Inventory || Cart</title>
       </Helmet>
       <div className="flex justify-between w-full pe-4">
         <h1 className="text-3xl font-bold">
-          Checkout Cart: {cartItems?.soldQuantity}
+          Cart Quantity: {cartItems?.soldQuantity}
         </h1>
         <button
-          className="btn btn-success text-base"
-          disabled={!isLoading && cartItems.soldQuantity < 1}
+          className={`btn btn-success text-white hover:text-black ${
+            isLoading && "w-44 h-12"
+          }`}
+          disabled={isLoading || cartItems.soldQuantity < 1}
         >
-          <Link to="/dashboard/checkout">Proceed Checkout</Link>
+          {isLoading ? (
+            <span className="loading loading-spinner loading-md"></span>
+          ) : (
+            <Link to="/dashboard/checkout">Proceed to Checkout</Link>
+          )}
         </button>
       </div>
       <div className="divider my-1"></div>
-      <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {cartItems?.totalItems?.map((product) => (
-          <div
-            key={product._id}
-            className="card card-compact w-full bg-base-100 shadow-xl"
-          >
-            <figure>
-              <img src={product?.productLogoURL} alt="Product image" />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title"> {product?.productName} </h2>
-              <p className="text-base font-semibold">
-                Price: $ {product?.sellingPrice?.toFixed(2)}
-              </p>
-              <p className="text-base font-medium">
-                Stock: {product?.productQuantity}pcs
-              </p>
-              <p className="text-base font-medium">
-                InCart: {product?.soldQuantity}pcs
-              </p>
-            </div>
-          </div>
-        ))}
+      <div className="action-center flex w-full justify-between gap-4 items-center">
+        <div className="form-control">
+          <label className="cursor-pointer label">
+            <input
+              type="checkbox"
+              name="select-all"
+              id="select-all"
+              defaultChecked={false}
+              className="checkbox checkbox-info"
+            />
+            <span className="label-text ps-2 text-base">
+              Select All ({cartItems?.soldQuantity}){"items"}
+            </span>
+          </label>
+        </div>
+        <div className="delete text-2xl hover:text-red-400 cursor-pointer flex items-center">
+          <MdDelete />
+          <span className="text-base font-mono">DELETE</span>
+        </div>
+      </div>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>SN</th>
+              <th className="text-center">Item Details</th>
+
+              <th>Price</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cartItems?.totalItems?.map((product, idx) => (
+              <tr key={product._id}>
+                <th>{idx + 1}</th>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle w-12 h-12">
+                        <img
+                          src={product?.productLogoURL}
+                          alt="Product Image"
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="font-bold">{product?.productName}</div>
+                      <div className="text-sm opacity-50 w-9/12 text-justify">
+                        <span>
+                          Desc: {product?.productDescription}
+                          {", "}
+                        </span>
+                        <span> From: {product?.productLocation} </span>
+                      </div>
+                    </div>
+                  </div>
+                </td>
+
+                <td> ${product?.sellingPrice?.toFixed(2)} </td>
+                <th>
+                  <UnitHandler
+                    productStockQuantity={product?.productQuantity}
+                    defaultSelectedQuantity={product?.soldQuantity}
+                  />
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
