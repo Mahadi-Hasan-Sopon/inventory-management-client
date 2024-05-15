09d9ac6ee08c4d389be602c65c9f0b30e2148960
Mahadi Hasan <mahadih137@gmail.com>
2024-05-15 11:11:11 +0600

    base url for query dynamicaly adding by env file

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,14 +1,19 @@
 import axios from "axios";
 
+const baseURL =
+  import.meta.env.VITE_DEVELOPMENT_MODE === "true"
+    ? "http://localhost:5000"
+    : "https://inventory-management-server-chi.vercel.app";
+
 export const axiosPublic = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: baseURL,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
 export const axiosSecure = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: baseURL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
