a23524933f4fc51ebbfbf047897fbc20e44b511e
Mahadi Hasan <mahadih137@gmail.com>
2024-05-16 14:46:23 +0600

    user & shop delete functionality added for admin

--- src/pages/dashboard/adminRoutes/ManageShops.jsx
+++ src/pages/dashboard/adminRoutes/ManageShops.jsx
@@ -1,51 +1,184 @@
+/* eslint-disable react/prop-types */
 import { useQuery } from "@tanstack/react-query";
 import { axiosSecure } from "../../../hooks/useAxios";
 import { Helmet } from "react-helmet";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { FiDelete, FiEdit } from "react-icons/fi";
 
-const ManageShops = () => {
-  const { data: allShop } = useQuery({
+const ManageShops = ({ itemsPerPage = 10 }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [deletedShopId, setDeletedShopId] = useState(null);
+
+  const { data: allShop, refetch: refetchShops } = useQuery({
     queryKey: ["allShop"],
     queryFn: () => axiosSecure.get("/allShop").then((res) => res.data),
   });
 
-  //   console.log({ allShop });
+  // console.log(allShop);
+
+  // calculate index of shops to show current page
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = allShop?.slice(indexOfFirstItem, indexOfLastItem);
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+  const totalPages = Math.ceil(allShop?.length / itemsPerPage);
+
+  const handleDeleteShopClick = async () => {
+    // console.log(deletedShopId);
+    const toastId = toast.loading("Deleting shop...");
+
+    try {
+      const response = await axiosSecure.delete(
+        `/admin/allShop/${deletedShopId}`
+      );
+
+      if (response.status === 200 && response.data?.deletedCount > 0) {
+        toast.success("shop deleted successfully", { id: toastId });
+        refetchShops();
+        setDeletedShopId(null);
+      }
+    } catch (err) {
+      console.error(err);
+      toast.error("Failed to delete shop", { id: toastId });
+    }
+  };
 
   return (
     <div>
       <Helmet>
         <title>Inventory || Manage Shops</title>
       </Helmet>
-      <h1 className="text-2xl font-bold">Manage Shops - {allShop?.length}</h1>
-      <div className="overflow-x-auto my-10">
-        <table className="table table-zebra">
-          <thead className="text-center text-lg">
-            <tr>
-              <th>#</th>
-              <th>Shop Logo</th>
-              <th>Shop Name</th>
-              <th>Product Limit</th>
-              <th>Shop Description</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {allShop?.map((shop, index) => (
-              <tr key={index + 1} className=" text-center text-base">
-                <th>{index + 1}</th>
-                <td className="flex justify-center">
-                  <img className="w-20" src={shop.shopLogo} alt="" />
-                </td>
-                <td> {shop.shopName} </td>
-                <td>{shop.productLimit}</td>
-                <td>{shop.shopDescription}</td>
-                <td>
-                  <button className="btn btn-info px-5">Send Notice</button>
-                </td>
+
+      <div className="border rounded py-6 flex flex-col min-h-[65vh]">
+        <h2 className="text-lg font-bold text-slate-800 mb-6 px-4">
+          Manage Shops - {allShop?.length}
+        </h2>
+        <div className="overflow-x-auto">
+          <table className="table">
+            <thead className="text-center">
+              <tr>
+                <th>#</th>
+                <th>Shop Logo</th>
+                <th>Shop Name</th>
+                <th>Product Limit</th>
+                <th>Shop Description</th>
+                <th>Actions</th>
               </tr>
+            </thead>
+            <tbody>
+              {currentItems?.map((shop) => (
+                <tr
+                  key={allShop.indexOf(shop) + 1}
+                  className=" text-center text-base"
+                >
+                  <th>{allShop.indexOf(shop) + 1}</th>
+                  <td className="p-2">
+                    <img
+                      className="w-20 max-h-20 object-contain"
+                      src={shop.shopLogo}
+                      alt="Shop Logo"
+                    />
+                  </td>
+                  <td>{shop.shopName}</td>
+                  <td>{shop.productLimit}</td>
+                  <td>{shop.shopDescription}</td>
+                  <td>
+                    <div className="flex gap-2 justify-center items-center">
+                      <button className="bg-[#FE9F43] py-2 text-lg px-3 rounded-md text-white hover:cursor-pointer hover:bg-primary group">
+                        <FiEdit className="group-hover:scale-125" />
+                      </button>
+                      <button
+                        className="bg-[#FE9F43] py-2 text-lg px-3 rounded-md text-white hover:cursor-pointer hover:bg-red-500 group"
+                        onClick={() => {
+                          document
+                            .getElementById("delete_Confirmation_Modal")
+                            .showModal();
+                          setDeletedShopId(shop._id);
+                        }}
+                      >
+                        <FiDelete className="group-hover:scale-150" />
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+
+        <div className="flex-grow"></div>
+        <div className="divider"></div>
+
+        {/* pagination */}
+        <div className="flex justify-center w-full">
+          <div className="join">
+            <button
+              className="join-item btn"
+              disabled={currentPage - 1 <= 0}
+              onClick={() => {
+                if (currentPage - 1 > 0) {
+                  setCurrentPage((curr) => curr - 1);
+                }
+              }}
+            >
+              «
+            </button>
+            {Array.from({ length: totalPages }).map((_, idx) => (
+              <input
+                key={idx}
+                className="join-item btn btn-square"
+                type="radio"
+                name="options"
+                aria-label={idx + 1}
+                checked={currentPage === idx + 1}
+                readOnly
+                onClick={() => handlePageChange(idx + 1)}
+              />
             ))}
-          </tbody>
-        </table>
+            <button
+              className="join-item btn"
+              disabled={currentPage + 1 > totalPages}
+              onClick={() => {
+                if (currentPage + 1 <= totalPages) {
+                  setCurrentPage((curr) => curr + 1);
+                }
+              }}
+            >
+              »
+            </button>
+          </div>
+        </div>
       </div>
+      {/* delete confirmation modal */}
+      <dialog
+        id="delete_Confirmation_Modal"
+        className="modal modal-bottom sm:modal-middle"
+      >
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">Delete!</h3>
+          <p className="py-4">Are you sure you want to delete the shop ?</p>
+          <div className="modal-action">
+            <form method="dialog">
+              <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                ✕
+              </button>
+              <div className="flex gap-6 justify-end items-center">
+                <button className="btn btn-sm btn-info">Cancel</button>
+                <button
+                  className="btn btn-sm btn-error"
+                  onClick={handleDeleteShopClick}
+                >
+                  Delete
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </dialog>
     </div>
   );
 };
--- src/pages/dashboard/adminRoutes/ManageUsers.jsx
+++ src/pages/dashboard/adminRoutes/ManageUsers.jsx
@@ -3,16 +3,17 @@ import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 import { Helmet } from "react-helmet";
 import { axiosSecure } from "../../../hooks/useAxios";
+import { FiEdit, FiDelete } from "react-icons/fi";
+import toast from "react-hot-toast";
 
-const ManageUsers = ({ itemsPerPage = 3 }) => {
+const ManageUsers = ({ itemsPerPage = 10 }) => {
   const [currentPage, setCurrentPage] = useState(1);
+  const [deletedUserId, setDeletedUserId] = useState(null);
 
-  const { data: users } = useQuery({
+  const { data: users, refetch: refetchUsers } = useQuery({
     queryKey: ["allUsers"],
     queryFn: () => axiosSecure.get("/admin/allUser").then((res) => res.data),
   });
-
-  // console.log({ users });
 
   // calculate index of users to show current page
   const indexOfLastItem = currentPage * itemsPerPage;
@@ -24,12 +25,32 @@ const ManageUsers = ({ itemsPerPage = 3 }) => {
   };
   const totalPages = Math.ceil(users?.length / itemsPerPage);
 
+  const handleDeleteUserClick = async () => {
+    // console.log(deletedUserId);
+    const toastId = toast.loading("Deleting user...");
+
+    try {
+      const response = await axiosSecure.delete(
+        `/admin/allUser/${deletedUserId}`
+      );
+
+      if (response.status === 200 && response.data?.deletedCount > 0) {
+        toast.success("User deleted successfully", { id: toastId });
+        refetchUsers();
+        setDeletedUserId(null);
+      }
+    } catch (err) {
+      console.error(err);
+      toast.error("Failed to delete user", { id: toastId });
+    }
+  };
+
   return (
     <div>
       <Helmet>
         <title>Inventory || Manage Users</title>
       </Helmet>
-      <div className="sold-products border rounded py-6 flex flex-col">
+      <div className="border rounded py-6 flex flex-col min-h-[65vh]">
         <h2 className="text-lg font-bold text-slate-800 mb-6 px-4">Users</h2>
         <div className="overflow-x-auto">
           <table className="table">
@@ -44,24 +65,33 @@ const ManageUsers = ({ itemsPerPage = 3 }) => {
               </tr>
             </thead>
             <tbody>
-              {currentItems?.map((user, index) => (
+              {currentItems?.map((user) => (
                 <tr
-                  key={index + 1}
+                  key={users.indexOf(user) + 1}
                   className="text-center hover hover:cursor-pointer"
                 >
-                  <td> {index + 1} </td>
+                  <td> {users.indexOf(user) + 1} </td>
                   <td> {user?.name} </td>
                   <td>{user?.email}</td>
                   <td> {user?.shopName ? user.shopName : "No Shop"} </td>
                   <td> {user?.role ? user.role : "User"} </td>
                   <td>
-                    {!user?.role && !user?.shopName ? (
-                      <button className="bg-[#FE9F43] py-2 text-xs px-3 rounded-md text-white">
-                        Send Offer
+                    <div className="flex gap-2 justify-center items-center">
+                      <button className="bg-[#FE9F43] py-2 text-lg px-3 rounded-md text-white hover:cursor-pointer hover:bg-primary group">
+                        <FiEdit className="group-hover:scale-125" />
                       </button>
-                    ) : (
-                      ""
-                    )}
+                      <button
+                        className="bg-[#FE9F43] py-2 text-lg px-3 rounded-md text-white hover:cursor-pointer hover:bg-red-500 group"
+                        onClick={() => {
+                          document
+                            .getElementById("delete_Confirmation_Modal")
+                            .showModal();
+                          setDeletedUserId(user._id);
+                        }}
+                      >
+                        <FiDelete className="group-hover:scale-150" />
+                      </button>
+                    </div>
                   </td>
                 </tr>
               ))}
@@ -71,6 +101,8 @@ const ManageUsers = ({ itemsPerPage = 3 }) => {
 
         <div className="flex-grow"></div>
         <div className="divider"></div>
+
+        {/* pagination */}
         <div className="flex justify-center w-full">
           <div className="join">
             <button
@@ -110,6 +142,32 @@ const ManageUsers = ({ itemsPerPage = 3 }) => {
           </div>
         </div>
       </div>
+      {/* delete confirmation modal */}
+      <dialog
+        id="delete_Confirmation_Modal"
+        className="modal modal-bottom sm:modal-middle"
+      >
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">Delete!</h3>
+          <p className="py-4">Are you sure you want to delete the user ?</p>
+          <div className="modal-action">
+            <form method="dialog">
+              <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                ✕
+              </button>
+              <div className="flex gap-6 justify-end items-center">
+                <button className="btn btn-sm btn-info">Cancel</button>
+                <button
+                  className="btn btn-sm btn-error"
+                  onClick={handleDeleteUserClick}
+                >
+                  Delete
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </dialog>
     </div>
   );
 };
